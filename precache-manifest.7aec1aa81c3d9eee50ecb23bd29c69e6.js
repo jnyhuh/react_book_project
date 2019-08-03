425411c0340ef8c5c67257bd01331ef1fbbccb9a
afgc <afgc@afgcs-air.home>
2019-08-03 17:29:14 -0500

    Updates

---
+++ precache-manifest.7aec1aa81c3d9eee50ecb23bd29c69e6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485d85b8dcca46ad3e502910be350098",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/index.html"
+  },
+  {
+    "revision": "49a97bb5ccc1c1752079",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/css/main.2b360685.chunk.css"
+  },
+  {
+    "revision": "e13e393f06ac4be38071",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/js/2.27031efd.chunk.js"
+  },
+  {
+    "revision": "49a97bb5ccc1c1752079",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/js/main.6b8675f0.chunk.js"
+  },
+  {
+    "revision": "897eb28971292eacf6aa",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/js/runtime~main.0d7d5e2b.js"
+  },
+  {
+    "revision": "1f7beca5bf4cda1803cd59cd881c372d",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/media/100_1.1f7beca5.jpg"
+  },
+  {
+    "revision": "82e7c41e073394f808fd382c97e71884",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/media/100_2.82e7c41e.jpg"
+  },
+  {
+    "revision": "6e25d86d33b97ab231889fd4a38530b4",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/media/100_3.6e25d86d.jpg"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "fd53372585bf0ef377e97ee9dd7ea925",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/media/100_5.fd533725.jpg"
+  },
+  {
+    "revision": "af5261ee56766966f8ccef1f8d6f0552",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/media/educated.af5261ee.jpg"
+  },
+  {
+    "revision": "deb7a0b688cbe42a7cd29a2f9ced789c",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/media/guitar.deb7a0b6.jpg"
+  },
+  {
+    "revision": "b175e1c4790112fe609a0f3279e8535d",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/media/logo_200.b175e1c4.png"
+  },
+  {
+    "revision": "f8be41d69a5bf44698dde84ddf01fa0e",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/media/mockingbird.f8be41d6.jpg"
+  },
+  {
+    "revision": "5a69d90bc2578dd9c3f230a993e6dafd",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/media/reckoning.5a69d90b.jpg"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "https://jnyhuh.github.io/react_book_project_capstone/static/media/sidebar-4.80d4a4e5.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://jnyhuh.github.io/react_book_project_capstone/precache-manifest.7aec1aa81c3d9eee50ecb23bd29c69e6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://jnyhuh.github.io/react_book_project_capstone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
